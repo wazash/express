4ec28e1384a6fae7ee82d51573dcd9c8febebd46
Mateusz 'Wazash' Witek <lazasz.email@gmail.com>
2019-06-19 12:02:41 +0200

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+  console.log(req.query);
+  const search = req.query.search || '';
+
+  const findNews = News
+    .find({ title: new RegExp(search.trim(), 'i') })
+    .sort({ created: -1 })
+    ;
+
+  findNews.exec((err, data) => {
+
+    res.render('news', { title: 'News', data, search });
+  });
+
 });
 
 
